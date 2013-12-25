584fd40eb727a2a911711db54f65ebb45048566a
John Johnson <dampboxdev@gmail.com>
2013-12-24 16:37:38 -0800

    front end framework

--- app/models/owner.rb
+++ app/models/owner.rb
@@ -1,20 +1,7 @@
 class Owner
 
 	def name
-		name = 'Beatrice Fallsalot'
+		@owner = Owner.new
 	end
-
-	def birthdate
-		birthdate = Date.new(1902, 01, 28)
-	end
-
-	def countdown
-		today = Date.today
-		birthday = Date.new(today.year, birthdate.month, birthdate.day)
-		if birthday > today
-			countdown = (birthday-today).to_i
-		else
-			countdown = (birthday.next_year - today).to_i
-		end
-	end
+	
 end
